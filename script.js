fc4f3d88ef268dac955bab9bb6edb4d248eb01fa
Adriel Medeiros <130292427+Adrielmedeiros01@users.noreply.github.com>
2026-02-02 02:20:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-.
+function revealOnScroll() {
+    const elements = document.querySelectorAll('#reveal');
+
+    elements.forEach(el => {
+        const windowHeight = window.innerHeight;
+        const elementTop = el.getBoundingClientRect().top;
+        const elementVisible = 100;
+
+        if (elementTop < windowHeight - elementVisible) {
+            el.classList.add('active');
+        }
+    });
+}
+
+window.addEventListener('scroll', revealOnScroll);
+revealOnScroll(); // roda ao carregar;
+
+
+
+function clickMenu(){ 
+    const mostra = document.getElementById("mostrar");      
+
+    if(mostra.style.display === "block"){
+        mostra.style.display = "none";        
+    } else {
+        mostra.style.display = "block";
+    }  
+}
+
+function mudoutamanho() {
+    const mostra = document.getElementById("mostrar");
+
+    if (window.innerWidth >= 768) {
+        mostra.style.display = "flex";                      
+    } else {                        
+        mostra.style.display = "none";
+    }
+}
+
+window.onload = mudoutamanho;
+window.onresize = mudoutamanho;
